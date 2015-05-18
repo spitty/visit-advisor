6f0f249c13f3455d0233997281d0153c80b83707
Stas <stas.ponkrashov@gmail.com>
2015-05-18 15:00:51 +0300

    Connection to database added at EventService class constructor

--- src/main/java/org/ipccenter/visitadvisor/bean/EventService.java
+++ src/main/java/org/ipccenter/visitadvisor/bean/EventService.java
@@ -8,7 +8,10 @@ import java.util.Collections;
 import java.util.List;
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
+
+import org.apache.log4j.Logger;
 import org.ipccenter.visitadvisor.model.Event;
+import java.sql.*;
 
 /**
  *
@@ -17,6 +20,26 @@ import org.ipccenter.visitadvisor.model.Event;
 @ManagedBean
 @ApplicationScoped
 public class EventService {
+    private static final Logger log = Logger.getLogger(EventContainer.class);
+    Connection connection;
+
+    public EventService() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            log.debug("Driver loaded");
+        } catch (ClassNotFoundException e) {
+            log.error("Can't load driver");
+            e.printStackTrace();
+        }
+        try {
+            this.connection = DriverManager.getConnection
+                            ("jdbc:mysql://localhost:3306/mysql", "eventsmanager", "Staddy32");
+            log.debug("Connected to database");
+        } catch (SQLException e) {
+            log.error("Can't connect to database");
+            e.printStackTrace();
+        }
+    }
 
     /**
      * Generate sample list of events
@@ -37,4 +60,7 @@ public class EventService {
         return events;
     }
 
+    public List<Event> getEvents() {
+        return new ArrayList<Event>();
+    }
 }
